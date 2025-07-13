654a369135ecc7000d8eeae987cfa4b06933f3e3
Darko Grubic <grubic@gmail.com>
2025-07-13 16:05:09 +0200

    display set & game loop commit

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pygame
-
-
+from constants import *
 
 def main():
+    pygame.init()
+
+    screen_tuple = (SCREEN_WIDTH, SCREEN_HEIGHT)
+    screen = pygame.display.set_mode(screen_tuple) # OVO JE BITNA LINIJA
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        
 
 
 if __name__ == "__main__":
