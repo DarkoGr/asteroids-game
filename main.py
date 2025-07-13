ae43ab695672adaa2edf579bdb7072bbe58d5cb2
Darko Grubic <grubic@gmail.com>
2025-07-13 21:06:22 +0200

    adding player commit

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
+    FPS_clock = pygame.time.Clock()
+    dt = 0
+
+
     screen_tuple = (SCREEN_WIDTH, SCREEN_HEIGHT)
-    screen = pygame.display.set_mode(screen_tuple) # OVO JE BITNA LINIJA
+    screen = pygame.display.set_mode(screen_tuple) 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+
+    player = Player(x, y)
 
     while True:
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = FPS_clock.tick(60) / 1000
+             
 
         
 
